56472aefdd562c6e713c403d8bece89d45ace6a4
Kent Riemondy <kent.riemondy@gmail.com>
2017-07-25 14:48:32 -0600

    htslib faidx assumes input region is one based, make zerobased

--- src/getSeq.cpp
+++ src/getSeq.cpp
@@ -18,30 +18,30 @@ DataFrame getSeq(DataFrame df, std::string fapath) {
   IntegerVector starts = df["start"] ;
   IntegerVector ends = df["end"] ;
 
+  // load fasta index
   faidx_t *fai;
   fai = fai_load(cfapath);
   if (fai == NULL) stop("can't load faidx file " + fapath);
 
+  // build sequence and header vectors for each bed entry
   for(int i = 0; i<nr ; i++) {
     auto chrom = as<std::string>(chroms[i]) ;
-    auto start = std::to_string(starts[i]) ;
-    auto end = std::to_string(ends[i]) ;
+    auto start = std::to_string(starts[i] + 1) ; // htslib assumes 1 based if input as region
+    auto end = std::to_string(ends[i] + 1) ;
 
     auto region = std::string(chrom + std::string(":") + start + std::string("-") + end) ;
-    Rcpp::Rcout << region << std::endl;
+    // pass region arg as c string
     auto reg = region.c_str() ;
     int seq_len ;
     char *seq = fai_fetch(fai, reg, &seq_len);
-    if (seq_len >= 0) {
-
+    if (seq_len >= 0) { // <0 is error
       std::string seq_out(seq, seq_len) ; // might not be necessary
-      Rcpp::Rcout << seq << " sequence" ;
       outseqs[i] = seq ;
       outseqnames[i] = region ;
     } else {
       stop("sequence not found for " + region) ;
     }
-    free(seq);
+    free(seq); //necessary to prevent memory leak
   }
 
   fai_destroy(fai) ;
@@ -49,10 +49,12 @@ DataFrame getSeq(DataFrame df, std::string fapath) {
   return DataFrame::create(_("header") = outseqnames,
                            _("seq") = outseqs,
                            _("stringsAsFactors") = false) ;
-
 }
 
 /*** R
-
-getSeq(df, "/Users/kriemo/Projects/shared_dbases/genomes/GRCm38.p4.genome.fa")
+df <- tibble::tribble(
+  ~chrom, ~start, ~end,
+  "JH393279.1", 14559891, 14559892
+)
+getSeq(df, "/Users/kriemo/Projects/Martin/dbases/ensembl85/Ictidomys_tridecemlineatus.spetri2.dna.toplevel.fa")
 */
